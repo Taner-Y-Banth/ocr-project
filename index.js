54529b111fe7af7e56e31b493aee587a1eb2600d
Taner-Y-Banth <62570956+Taner-Y-Banth@users.noreply.github.com>
2021-11-17 17:10:27 -0800

    succesfully runs ocr on image in image folder then sends on ocr channel

---
+++ index.js
@@ -0,0 +1,40 @@
+import { createWorker } from 'tesseract.js';
+import ws from 'ws';
+import { NstrumentaClient } from 'nstrumenta';
+import fs from 'fs';
+import { readFile } from 'fs/promises';
+
+const nstrumenta = new NstrumentaClient({ hostUrl: 'ws://localhost:8088' });
+const completed = []
+
+fs.watch('./images', async (eventType, filename) => {
+  console.log(`event type is: ${eventType}`);
+  if (filename.endsWith(".jpg") && !completed.includes(filename) && eventType == 'change') {
+    completed.push(filename)
+    console.log(`filename provided: ${filename}`);
+    const worker = createWorker({
+      logger: m => console.log(m)
+    });
+    const languageFile = 'eng'
+    await worker.load();
+    await worker.loadLanguage(languageFile);
+    await worker.initialize(languageFile);
+    const imageFile = await readFile(`./images/${filename}`);
+    const { data: { text } } = await worker.recognize(imageFile);
+    console.log(text);
+    await worker.terminate();
+    console.log(text);
+    nstrumenta.send('ocr', text);
+    fs.rm('./eng.traineddata', () => { });
+  } else {
+    console.log('filename not provided');
+  }
+});
+
+nstrumenta.addListener("open", () => {
+  console.log("websocket successfully opened")
+});
+
+console.log("nstrumenta init")
+
+nstrumenta.init(ws);
